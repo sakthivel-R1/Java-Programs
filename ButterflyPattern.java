01ef2aee1f1af8d9267ed276a5bf6f8945b4284b
sakthivel-R1 <sakthivelriot2427@ksrce.ac.in>
2025-09-20 10:42:46 +0530

    Modified

---
+++ ButterflyPattern.java
@@ -0,0 +1,43 @@
+public class ButterflyPattern{
+    public static void main(String[] args) {
+        int n = 5; // number of rows for half of the butterfly
+
+        // Upper half
+        for (int i = 1; i <= n; i++) {
+            // Left stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            // Spaces
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+
+            // Right stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower half
+        for (int i = n; i >= 1; i--) {
+            // Left stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            // Spaces
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+
+            // Right stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
